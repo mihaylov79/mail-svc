3efe1f0ab7fce81da4712735dfe1ac48d51446e2
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-03-14 15:42:26 +0200

    NotificationService - sendMail method added

--- src/main/java/mail_svc/service/NotificationService.java
+++ src/main/java/mail_svc/service/NotificationService.java
@@ -1,10 +1,16 @@
 package mail_svc.service;
 
+import jakarta.mail.internet.MimeMessage;
+import mail_svc.model.Notification;
 import mail_svc.model.NotificationPreference;
+import mail_svc.model.NotificationStatus;
 import mail_svc.repository.NotificationPreferenceRepository;
 import mail_svc.repository.NotificationRepository;
 import mail_svc.web.dto.NotificationPreferenceRequest;
+import mail_svc.web.dto.NotificationRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -15,11 +21,13 @@ public class NotificationService {
 
     private final NotificationRepository notificationRepository;
     private final NotificationPreferenceRepository notificationPreferenceRepository;
+    private final JavaMailSender mailSender;
 
     @Autowired
-    public NotificationService(NotificationRepository notificationRepository, NotificationPreferenceRepository notificationPreferenceRepository) {
+    public NotificationService(NotificationRepository notificationRepository, NotificationPreferenceRepository notificationPreferenceRepository, JavaMailSender mailSender) {
         this.notificationRepository = notificationRepository;
         this.notificationPreferenceRepository = notificationPreferenceRepository;
+        this.mailSender = mailSender;
     }
 
     public NotificationPreference updateNotificationPreference(NotificationPreferenceRequest preferenceRequest) {
@@ -44,5 +52,51 @@ public class NotificationService {
 
         return notificationPreferenceRepository.findByRecipientId(recipientId)
                 .orElseThrow(() -> new NullPointerException("Статуса на нотофикациите за потребител с идентификация [%s]не беше намерен".formatted(recipientId)));
+    }
+
+
+    public Notification sendMailNotification(NotificationRequest notificationRequest){
+
+        UUID recipientId = notificationRequest.getRecipientId();
+
+        NotificationPreference recipientPreference = getPreferenceByRecipientById(recipientId);
+
+        if(!recipientPreference.isEnabled()){
+            throw new IllegalArgumentException("Нотификациите за  потребител с идентификация [%s] са изключени".formatted(recipientId));
+        }
+
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message,false,"UTF-8");
+            helper.setTo(recipientPreference.getInfo());
+            helper.setSubject(notificationRequest.getTitle());
+            helper.setText(notificationRequest.getContent());
+
+            mailSender.send(message);
+
+            Notification notification = Notification.builder()
+                    .subject(notificationRequest.getTitle())
+                    .content(notificationRequest.getContent())
+                    .created(LocalDateTime.now())
+                    .recipientId(notificationRequest.getRecipientId())
+                    .status(NotificationStatus.COMPLETED)
+                    .deleted(false)
+                    .build();
+
+             return notificationRepository.save(notification);
+        } catch (Exception e) {
+            Notification notification = Notification.builder()
+                    .subject(notificationRequest.getTitle())
+                    .content(notificationRequest.getContent())
+                    .created(LocalDateTime.now())
+                    .recipientId(notificationRequest.getRecipientId())
+                    .status(NotificationStatus.FAILED)
+                    .deleted(false)
+                    .build();
+
+            notificationRepository.save(notification);
+
+            throw new RuntimeException("Изпращането беше неуспешно!",e);
+        }
     }
 }
---
+++ src/main/java/mail_svc/web/dto/NotificationRequest.java
@@ -0,0 +1,21 @@
+package mail_svc.web.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class NotificationRequest {
+
+    @NotNull
+    private UUID recipientId;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
+}
