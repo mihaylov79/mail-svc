6dc3e334a8adf568adfa45fd3e47de53b783df32
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-03-27 16:27:00 +0200

    Testing Started

--- src/test/java/mail_svc/service/NotificationServiceUTest.java
+++ src/test/java/mail_svc/service/NotificationServiceUTest.java
@@ -1,12 +1,14 @@
 package mail_svc.service;
 
 
+import mail_svc.model.NotificationPreference;
 import mail_svc.repository.NotificationPreferenceRepository;
 import mail_svc.repository.NotificationRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -15,7 +17,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class NotificationServiceUTest {
@@ -40,6 +42,24 @@ public class NotificationServiceUTest {
         when(notificationPreferenceRepository.findByRecipientId(recipientId)).thenReturn(Optional.empty());
 
         assertThrows(NullPointerException.class,()-> notificationService.changeNotificationPreference(recipientId,enabled));
+
+    }
+
+    @Test
+    void given_ExistingNotificationPreference_when_changeNotificationPreference_then_NotificationIsChanged(){
+
+        UUID recipientId = UUID.randomUUID();
+        boolean enabled = true;
+
+        NotificationPreference preference = NotificationPreference.builder().enabled(false).build();
+        when(notificationPreferenceRepository.findByRecipientId(recipientId)).thenReturn(Optional.of(preference));
+
+        notificationService.changeNotificationPreference(recipientId,true);
+
+//        assertTrue(preference.isEnabled());
+        verify(notificationPreferenceRepository,times(1))
+                        .save(Mockito.argThat(NotificationPreference::isEnabled));
+
 
     }
 
