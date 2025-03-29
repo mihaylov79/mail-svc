b43400686a1d948d6691d682bcb9f010a5a5ee8c
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-03-30 01:53:29 +0200

    Tests Added

--- src/test/java/mail_svc/service/NotificationServiceUTest.java
+++ src/test/java/mail_svc/service/NotificationServiceUTest.java
@@ -19,6 +19,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -151,7 +153,6 @@ public class NotificationServiceUTest {
         MimeMessage mimeMessage = mock(MimeMessage.class);
         MimeMessageHelper helper = mock(MimeMessageHelper.class);
         when(mailSender.createMimeMessage()).thenReturn(mimeMessage);
-        when(mailSender.createMimeMessage()).thenReturn(mimeMessage);
 
         when(templateEngine.process(any(String.class), any())).thenReturn("<html><body>Test content</body></html>");
 
@@ -168,7 +169,92 @@ public class NotificationServiceUTest {
     }
 
 
+    @Test
+    void given_NotificationRequest_when_sendMailNotificationIfExceptionIsThrown_shouldFailToSendNotification() {
+        // Arrange
+        UUID recipientId = UUID.randomUUID();
 
+        NotificationPreference notificationPreference = NotificationPreference.builder()
+                .recipientId(recipientId)
+                .info("old@example.com")
+                .enabled(true).build();
+
+        NotificationRequest notificationRequest = NotificationRequest.builder()
+                .recipientId(recipientId)
+                .firstName("John")
+                .lastName("Doe")
+                .content("This is a test notification.")
+                .title("Test Notification")
+                .build();
+
+        when(notificationPreferenceRepository.findByRecipientId(recipientId))
+                .thenReturn(Optional.of(notificationPreference));  // Добавено мокване
+
+        when(mailSender.createMimeMessage()).thenThrow(new RuntimeException("Mail sending failed"));
+
+        // Act
+        notificationService.sendMailNotification(notificationRequest);
+
+        // Assert
+        verify(notificationRepository).save(argThat(n -> n.getStatus() == NotificationStatus.FAILED));
+    }
+
+    @Test
+    void given_NotificationPreferenceIsDisabled_when_sendMailNotification_shouldThrowException() {
+
+        NotificationPreference notificationPreference = NotificationPreference.builder()
+                .recipientId(UUID.randomUUID())
+                .info("old@example.com")
+                .enabled(false).build();
+
+
+        NotificationRequest notificationRequest = NotificationRequest.builder()
+                .recipientId(notificationPreference.getRecipientId())
+                .firstName("John")
+                .lastName("Doe")
+                .content("This is a test notification.")
+                .title("Test Notification")
+                .build();
+
+
+        when(notificationPreferenceRepository.findByRecipientId(any(UUID.class)))
+                .thenReturn(java.util.Optional.of(notificationPreference));
+
+
+        assertThrows(IllegalArgumentException.class, () -> notificationService.sendMailNotification(notificationRequest));
+    }
+
+    @Test
+    void givenHistory_when_clearNotificationHistory_thenShouldSetDeleteFieldTrue() {
+        // Arrange
+        UUID recipientId = UUID.randomUUID();
+        List<Notification> notifications = List.of(
+                Notification.builder().id(UUID.randomUUID()).recipientId(recipientId).deleted(false).build(),
+                Notification.builder().id(UUID.randomUUID()).recipientId(recipientId).deleted(false).build()
+        );
+
+        when(notificationRepository.findAllByRecipientIdAndDeleted(recipientId, false)).thenReturn(notifications);
+
+        // Act
+        notificationService.clearNotificationHistory(recipientId);
+
+        // Assert
+        for (Notification notification : notifications) {
+            verify(notificationRepository).save(argThat(n -> n.getId().equals(notification.getId()) && n.isDeleted()));
+        }
+    }
+
+    @Test
+    void given_schedulerEveryMonth_when_CleanUpOldNotifications_the_deletedShouldBeSetToTrue() {
+
+
+
+        when(notificationRepository.deleteByDeletedTrueAndCreatedBefore(any(LocalDateTime.class))).thenReturn(5);
+
+        notificationService.cleanUpOldNotifications();
+
+        verify(notificationRepository).deleteByDeletedTrueAndCreatedBefore(any(LocalDateTime.class));
+    }
 
 
 }
