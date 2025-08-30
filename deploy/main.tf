d431c0d4ecf309c130135f5afde1de95eff5023b
Mihaylov <misho.mihaylov@gmail.com>
2025-08-31 01:31:06 +0300

    Added Terraform config for deployment

---
+++ deploy/main.tf
@@ -0,0 +1,152 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.42.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  tenant_id = var.tenant_id
+  
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "mail-svc-rg"
+  location = "Switzerland North"
+}
+
+resource "azurerm_storage_account" "sa" {
+
+  name = "mailsvcstorage"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+
+}
+
+resource "azurerm_storage_share" "mysql" {
+  name  = "mysqlshare"
+  storage_account_id = azurerm_storage_account.sa.id
+  quota = 1
+}
+
+resource "azurerm_container_app_environment" "cae" {
+  location            = azurerm_resource_group.rg.location
+  name                = "mail-svc-env"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_container_app" "cadb" {
+  container_app_environment_id = azurerm_container_app_environment.cae.id
+  name                         = "mail-svc-db-container-app"
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+
+  secret {
+    name = "mailsvc_db_pass"
+    value = var.mailsvc_db_pass
+  }
+
+  template {
+    container {
+      cpu    = 1
+      image  = "mysql:8.0"
+      memory = "1Gi"
+      name   = "mailsvc_db"
+
+      env {
+        name = "MYSQL_ROOT_PASSWORD"
+        secret_name = "mailsvc_db_pass"
+      }
+
+      env{
+        name = "MYSQL_DATABASE"
+        value = "mailsvc_db"
+      }
+
+      env {
+        name = "MYSQL_USER"
+        value = "admin_user"
+      }
+
+      env {
+        name = "MYSQL_PASSWORD"
+        secret_name = "mailsvc_db_pass"
+      }
+
+      volume_mounts {
+        name = "mail_svc_data"
+        path = "/var/lib/mysql"
+      }
+    }
+    volume {
+      name = "mail_svc_data"
+      storage_name = azurerm_storage_account.sa.name
+      storage_type = "AzureFile"
+      share_name   = azurerm_storage_share.mysql.name
+    }
+
+  }
+
+  ingress {
+    external_enabled = false
+    target_port = 3306
+  }
+
+}
+
+resource "azurerm_container_app" "caapp" {
+  container_app_environment_id = azurerm_container_app_environment.cae.id
+  name                         = "mail-svc-app-container-app"
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+
+  secret {
+    name = "mailsvc_db_pass"
+    value = var.mailsvc_db_pass
+  }
+
+  template {
+    container {
+      cpu    = 1
+      image  = "mihaylov79/mail-svc:latest"
+      memory = "1Gi"
+      name   = "mail-svc-app"
+
+      env {
+        name  = "DB_HOST"
+        value = "${azurerm_container_app.cadb.name}.${azurerm_container_app_environment.cae.name}.internal"
+      }
+
+      env {
+        name = "DB_USER"
+        value = "admin_user"
+      }
+
+      env {
+        name = "DB_PASS"
+        secret_name = "mailsvc_db_pass"
+      }
+      env {
+        name = "DB_NAME"
+        value = "mailsvc_db"
+      }
+
+      env {
+        name  = "SPRING_PROFILES_ACTIVE"
+        value = "prod"
+      }
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port = 8080
+  }
+}
---
+++ deploy/variables.tf
@@ -0,0 +1,24 @@
+
+variable "subscription_id" {
+  type        = string
+  description = "subscription ID"
+}
+
+variable "tenant_id" {
+  type = string
+  description = "tenant ID"
+}
+
+variable "mailsvc_db_pass" {
+  type = string
+  description = "Mail-svc database password"
+}
+
+variable "image_1_name" {
+  type = string
+  description = "Docker image name"
+
+}variable "image_2_name" {
+  type = string
+  description = "Docker image name"
+}
