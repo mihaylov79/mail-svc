33a9db92bd79593fc862e9f7303185e5c2ee28a7
Mihaylov <misho.mihaylov@gmail.com>
2025-09-02 05:12:35 +0300

    updated

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -1,29 +1,9 @@
-
-variable "subscription_id" {
-  type        = string
-  description = "subscription ID"
-}
-
-variable "tenant_id" {
-  type = string
-  description = "tenant ID"
-}
-
 variable "mailsvc_db_root_pass" {
-  type = string
+  type        = string
   description = "Mail-svc database root password"
 }
 
 variable "mailsvc_db_user_pass" {
-  type = string
+  type        = string
   description = "Mail-svc database user password"
 }
-
-# variable "image_1_name" {
-#   type = string
-#   description = "Docker image name"
-#
-# }variable "image_2_name" {
-#   type = string
-#   description = "Docker image name"
-# }
