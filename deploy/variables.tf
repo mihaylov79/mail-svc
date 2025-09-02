e18752317ab3f9a77a1c72f27c63d23280ec2dfc
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-09-02 15:08:14 +0300

    updated

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -7,13 +7,3 @@ variable "mailsvc_db_user_pass" {
   type        = string
   description = "Mail-svc database user password"
 }
-
-variable "subscription_id" {
-  type = string
-  description = "Subscription ID"
-}
-
-variable "tenant_id" {
-  type = string
-  description = "Tenant ID"
-}
