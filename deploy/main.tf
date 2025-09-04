7df4cb868da74ed1706b08367ceffa9577e46868
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-09-04 16:43:06 +0300

    app-update main.tf - updated

--- deploy/main.tf
+++ deploy/main.tf
@@ -146,6 +146,12 @@ resource "azurerm_container_app" "caapp" {
     value = var.mailsvc_db_user_pass
   }
 
+  lifecycle {
+    ignore_changes = [
+      secret,
+    ]
+  }
+
   template {
     container {
       cpu    = 0.75
