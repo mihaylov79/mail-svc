e56b497040add21aa3140877886fe2e33084edc5
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-09-09 12:28:22 +0300

    updated - links

--- app-update/main.tf
+++ app-update/main.tf
@@ -48,8 +48,8 @@ resource "azurerm_container_app" "caapp" {
 
       env {
         name  = "DB_HOST"
-        value = "mail-svc-db-container-app.internal.orangeriver-27d083f7.switzerlandnorth.azurecontainerapps.io"
-        # value = "mail-svc-db-container-app.mail-svc-env.internal"
+        # value = "mail-svc-db-container-app.internal.orangeriver-27d083f7.switzerlandnorth.azurecontainerapps.io"
+        value = "mail-svc-db-container-app.mail-svc-env.internal"
       }
       env {
         name  = "DB_USER"
