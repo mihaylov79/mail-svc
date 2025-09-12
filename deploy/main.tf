00b3096241ddc3c93d94032b5868cce1255a39bc
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-09-12 15:30:01 +0300

    updated - links

--- deploy/main.tf
+++ deploy/main.tf
@@ -163,8 +163,7 @@ resource "azurerm_container_app" "caapp" {
 
       env {
         name  = "DB_HOST"
-        # value = "${azurerm_container_app.cadb.name}.${azurerm_container_app_environment.cae.name}.internal"
-        value = "mailsvc-db-container-app.internal"
+        value = "${azurerm_container_app.cadb.name}.${azurerm_container_app_environment.cae.name}.internal"
       }
       env {
         name  = "DB_USER"
